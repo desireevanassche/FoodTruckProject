8e53afe1811bfa4f5505d9046fb82adebe53b789
Desiree <desiree.vanassche@gmail.com>
2022-02-26 10:50:47 -0700

    Array & quit option complete.

--- src/com/skilldistillery/foodtruck/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -1,58 +1,66 @@
 package com.skilldistillery.foodtruck;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class FoodTruckApp {
 
+	static Scanner kb = new Scanner(System.in); // can the canner be static?
+
 	public static void main(String[] args) {
-		Scanner kb = new Scanner(System.in);
 
 		System.out.println("Welome to the international food truck cook off!");
 		System.out.println("You can rate up to 5 food trucks.");
 
 		FoodTruck[] foodTruckArr = new FoodTruck[5];
-		for (int count = 1; count < foodTruckArr.length; count++) {
-			System.out.println("Enter the name of the food truck you want to rate: ");
-			String name = kb.nextLine();
-			System.out.println("What type of food is served at " + name + " ?");
-			String foodType = kb.nextLine();
-			System.out.println("Out of 5 stars, how would you rate " + name + " ?");
-			int rating = kb.nextInt();
-			FoodTruck x = new FoodTruck(name, foodType, rating);
+		boolean quit = false;
 
+		while (quit != true) {
+			
+			for (int i = 0; i < foodTruckArr.length; i++) {
+				foodTruckArr[i] = new FoodTruck();
+				System.out.println("Enter the name of the food truck you want to rate: ");
+				foodTruckArr[i].setName(kb.nextLine());
+
+				System.out.println("What type of food is served at this food truck ?");
+				foodTruckArr[i].setFoodType(kb.nextLine());
+
+				System.out.println("Out of 5 stars, how would you rate this food truck?");
+				foodTruckArr[i].setRating(kb.nextDouble());
+				
+
+				System.out.println("Add more input or quit? \ntype \"quit\" to go to main menu or..");
+				String userQuit = kb.nextLine();
+
+			if  (userQuit.equals("quit")) {
+				quit = true;			
+				//menu();
+					
+
+			}
 		}
-	
-		
-
-//		FoodTruck truck1 = new FoodTruck();
-//		foodTruckArr[0] = truck1;
-//		truck1.setName(inTruck1);
-//		truck1.setFoodType(inType);
-//		truck1.setRating(inRate);
-//
-//		FoodTruck truck2 = new FoodTruck();
-//		foodTruckArr[1] = truck2;
-//		truck2.setName(inTruck1);
-//		truck1.setFoodType(inType);
-//		truck1.setRating(inRate);
-//
-//		FoodTruck truck1 = new FoodTruck();
-//		foodTruckArr[0] = truck1;
-//		truck1.setName(inTruck1);
-//		truck1.setFoodType(inType);
-//		truck1.setRating(inRate);
-//
-//		FoodTruck truck1 = new FoodTruck();
-//		foodTruckArr[0] = truck1;
-//		truck1.setName(inTruck1);
-//		truck1.setFoodType(inType);
-//		truck1.setRating(inRate);
-//
-//		FoodTruck truck1 = new FoodTruck();
-//		foodTruckArr[0] = truck1;
-//		truck1.setName(inTruck1);
-//		truck1.setFoodType(inType);
-//		truck1.setRating(inRate);
-
+	}
 }
 }
+
+//menu();
+//	public static void menu() {
+//		System.out.println("Please enter a number below to choose from the following menu: ");
+//		System.out.println("1. List all food trucks.");
+//		System.out.println("2. See Average rating of food trucks.");
+//		System.out.println("3. Display the highest- ranked food truck.");
+//		System.out.println("4. Quit application.");
+//		int menuChoice = kb.nextInt();
+//	
+//	if (menuChoice == 1) {
+//		///list all food truck names here 
+//	} 
+//	if (menuChoice == 2) {
+//		//list average rating here 
+//		
+//		if (menuChoice == 3) {
+//			//display highest ranked food truck 
+//		}
+//		if (menuChoice == 4) {
+//			//quit app 
+//		}
+//	}
