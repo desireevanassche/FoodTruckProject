36a7c9ff092fce4c01247595992e2d43a2ebdb52
Desiree <desiree.vanassche@gmail.com>
2022-02-27 17:29:38 -0700

    Fuctioning, but could use imporvments.

--- src/com/skilldistillery/foodtruck/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/FoodTruck.java
@@ -1,37 +1,75 @@
 package com.skilldistillery.foodtruck;
 
 public class FoodTruck {
-private String name; 
-private String foodType;
-private int uniqueId;
-private double rating;
+	private String name;
+	private String foodType;
+	private int uniqueId;
+	private static int truckId = 1;
+	private double rating;
+	private double avgRating;
 
-public FoodTruck(String name, String foodType, int rating) {
-	// TODO Auto-generated constructor stub
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getFoodType() {
-	return foodType;
-}
-public void setFoodType(String foodType) {
-	this.foodType = foodType;
-}
-public int getUniqueId() {
-	return uniqueId;
-}
-public void setUniqueId(int uniqueId) {
-	this.uniqueId = uniqueId;
-}
-public double getRating() {
-	return rating;
-}
-public void setRating(double rating) {
-	this.rating = rating;
-}
+	public FoodTruck() { // no arg
+
+	}
+
+	public FoodTruck(String name, String foodType, double rating) {
+		this.name = name;
+		this.rating = rating;
+		this.foodType = foodType;
+		truckId++;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public double getAvgRating() {
+		return avgRating;
+	}
+
+	public void setAvgRating(double avgRating) {
+		this.avgRating = avgRating;
+	}
+
+	public int getTruckId() {
+		return truckId;
+	}
+
+	public void setTruckId() {
+		this.uniqueId = truckId; // removed ++ 
+	}
+
+	public String toString() {
+		String output = "Name: " + name + " Food type: " + foodType + " Rating: " + rating + " Unique ID: " + uniqueId;
+		return output;
+	}
+
+	public int getUniqueId() {
+		return uniqueId;
+	}
+
+	public void setUniqueId(int uniqueId) {
+		this.uniqueId = uniqueId;
+	}
 
 }
